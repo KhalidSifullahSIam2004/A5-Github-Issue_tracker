f42c02c40c102139102a0983331cf0c2e77d7c86
KhalidSifullahSIam2004 <surjo1905@gmail.com>
2026-03-06 21:40:06 +0600

    login page design functionality done

---
+++ script/index.js
@@ -0,0 +1,10 @@
+const userName = document.getElementById("username");
+const password = document.getElementById("password");
+
+document.getElementById("signInBtn").addEventListener("click", () => {
+  if (userName.value === "admin" && password.value === "admin123") {
+    location.replace("./main.html");
+  } else {
+    alert("Login failed. Please check your username and password.");
+  }
+});
