126862bafc4dec049726ae2909f7dd93e80f2895
KhalidSifullahSIam2004 <surjo1905@gmail.com>
2026-03-07 23:05:42 +0600

    checl a done

--- script/index.js
+++ script/index.js
@@ -3,6 +3,7 @@ const password = document.getElementById("password");
 
 document.getElementById("signInBtn").addEventListener("click", () => {
   if (userName.value === "admin" && password.value === "admin123") {
+    alert("Login successful!");
     location.replace("./main.html");
   } else {
     alert("Login failed. Please check your username and password.");
